6c5ed3635f498b44fcbfa1d08f22125b810d59ca
Dustin <adustin70@gmail.com>
2021-02-17 12:47:11 -0500

    Added DbSet property for SuperHero model in Data folder

--- SuperHeroProject/Data/ApplicationDbContext.cs
+++ SuperHeroProject/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SuperHeroProject.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,5 +13,7 @@ namespace SuperHeroProject.Data
             : base(options)
         {
         }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
     }
 }
